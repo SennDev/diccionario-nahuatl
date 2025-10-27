284e76df2dc9152667670988ba23709e564fd3d9
Senn <GersonEM8900@outlook.com>
2025-10-26 19:50:50 -0600

    reorganizancion

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // ¡¡AÑADE ESTA LÍNEA!!
   // Le dice a Vite que el proyecto vivirá en el subdirectorio /diccionario-nahuatl/
   base: '/diccionario-nahuatl/',
 })
